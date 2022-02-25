a5a6ee5051fd6fd87b74d9da76fc60b55dc49b72
mkemal80 <experimus1@gmail.com>
2022-02-25 16:41:54 +0300

    Chapter-1

---
+++ Chapter-1/exercise1.2.1.py
@@ -0,0 +1,13 @@
+# Exercise 1-2.
+
+"""
+1. How many seconds are there in 42 minutes 42 seconds?
+
+    1 minute is 60 seconds.
+
+"""
+
+
+second = 1
+minute = 60*second
+print(f"There are {42*minute + 42} seconds in 42 minutes and 42 seconds")

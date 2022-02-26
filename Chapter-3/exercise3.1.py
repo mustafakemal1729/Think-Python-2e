963e8a676143768a35550efe60af952ca1a822e7
mkemal80 <experimus1@gmail.com>
2022-02-26 22:47:35 +0300

    'Chapter-3'

---
+++ Chapter-3/exercise3.1.py
@@ -0,0 +1,15 @@
+# Exercise 3-1.
+
+"""
+Write a function named right_justify that takes a string named s as a parameter and prints the string with enough leading spaces 
+so that the last letter of the string is in column 70 of the display:
+>>> right_justify('monty')
+                                                                 monty
+
+Hint: Use string concatenation and repetition. Also, Python provides a built-in func‐
+tion called len that returns the length of a string, so the value of len('monty') is 5.
+
+"""
+
+def right_justify(s:str):
+    print(' ' * (70 - len(s)) + s)
---
+++ Chapter-3/exercise3.3.py
@@ -0,0 +1,75 @@
+# # Exercise 3-3.
+
+"""
+Note: This exercise should be done using only the statements and other features we
+have learned so far.
+
+1. Write a function that draws a grid like the following:
+
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+
+Hint: to print more than one value on a line, you can print a comma-separated sequence of values:
+    print('+', '-')
+
+By default, print advances to the next line, but you can override that behavior
+and put a space at the end, like this:
+    print('+', end=' ')
+    print('-')
+
+The output of these statements is '+ -'.
+A print statement with no argument ends the current line and goes to the next line.
+
+2. Write a function that draws a similar grid with four rows and four columns.
+
+"""
+
+# Solution for exercise 3.3.1
+
+def draw_grid():
+    draw_horizontal()
+    draw_vertical()
+    draw_horizontal()
+    draw_vertical()
+    draw_horizontal()
+
+def draw_horizontal():
+    print("+ - - - - + - - - - +")
+
+def draw_vertical():
+    print('|         |         |\n'*4, end='')
+
+
+draw_grid()
+print()
+
+# Solution for exercise 3.3.2
+
+def draw_four_by_four_grid():
+    draw_new_horizontal()
+    draw_new_vertical()
+    draw_new_horizontal()
+    draw_new_vertical()
+    draw_new_horizontal()
+    draw_new_vertical()
+    draw_new_horizontal()
+    draw_new_vertical()
+    draw_new_horizontal()
+
+def draw_new_horizontal():
+    print('+ - - - - + - - - - + - - - - + - - - - +')
+
+def draw_new_vertical():
+    print('|         |         |         |         |\n'*4, end='')
+
+
+draw_four_by_four_grid()

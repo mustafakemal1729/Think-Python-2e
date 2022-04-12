0baafff783f0aec694559339b80cb2ae99d9e7e0
mkemal80 <experimus1@gmail.com>
2022-04-12 18:27:32 +0300

    'Chapter-5'

--- Chapter-5/exercise5.6.py
+++ Chapter-5/exercise5.6.py
@@ -11,14 +11,13 @@ To draw a Koch curve with length x, all you have to do is:
     5. Draw a Koch curve with length x/3.
 
 
-    The exception is if x is less than 3: in that case, you can just draw a straight line with
-    length x.
-    1. Write a function called koch that takes a turtle and a length as parameters, and
-    that uses the turtle to draw a Koch curve with the given length.
-    2. Write a function called snowflake that draws three Koch curves to make the outline of a snowflake.
-    Solution: http://thinkpython2.com/code/koch.py.
-    3. The Koch curve can be generalized in several ways. See http://en.wikipedia.org/
-    wiki/Koch_snowflake for examples and implement your favorite.
+The exception is if x is less than 3: in that case, you can just draw a straight line with length x.
+1. Write a function called koch that takes a turtle and a length as parameters, and that uses the turtle 
+to draw a Koch curve with the given length.
+2. Write a function called snowflake that draws three Koch curves to make the outline of a snowflake.
+Solution: http://thinkpython2.com/code/koch.py.
+3. The Koch curve can be generalized in several ways. See http://en.wikipedia.org/wiki/Koch_snowflake 
+for examples and implement your favorite.
 """
 
 import turtle
@@ -26,7 +25,6 @@ import turtle
 bob = turtle.Turtle()
 
 def koch_curve(t, length):
-
     if length < 3:
         t.forward(length)
         return
@@ -42,14 +40,12 @@ def koch_curve(t, length):
     koch_curve(t, new_length)
 
 
+# Write a function called snowflake that draws three Koch curves to make the outline of a snowflake.
 def snowflake(t, length):
-    # Write a function called snowflake that draws three Koch curves to make the outline of a snowflake.
     for i in range(3):
         koch_curve(t, length)
         t.right(120)
 
 
 koch_curve(bob, 100)
-
-
 turtle.mainloop()

b1bfaaed31cb86ebb2eca080b8e10a5042843265
mkemal80 <experimus1@gmail.com>
2022-04-12 23:33:41 +0300

    'Chapter-3'

--- Chapter-3/exercise3.2.py
+++ Chapter-3/exercise3.2.py
@@ -24,6 +24,7 @@ Here's an example that uses do_twice to call a function named print_spam twice:
 
 5. Define a new function called do_four that takes a function object and a value and calls the function four times, passing the value as a parameter. 
 There should be only two statements in the body of this function, not four.
+
 """
 
 def do_twice(func, arg):
